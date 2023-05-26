8fa8da93f5b8f84c89b44e753dffa218a91797a5
aaron <xietianle@outlook.com>
2023-05-26 23:01:37 +1000

    change the host

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
-    BACKEND: 'http://10.249.31.220:8000', // Update this to match your Django server URL
-    DJ_END: 'http://10.249.31.220:8000',
-    PEER_HOST:'peerjs.aaron404.com',
-    HOST:'10.249.31.220',
-    SOCKET_HOST: 'http://10.249.31.220:8000' // Update this to match your Node server URL
+  BACKEND: "https://aaron404.com", // Update this to match your Django server URL
+  DJ_END: "https://aaron404.com",
+  PEER_HOST: "peerjs.aaron404.com",
+  HOST: "aaron404.com",
+  SOCKET_HOST: "https://aaron404.com", // Update this to match your Node server URL
 };
 
 export default config;

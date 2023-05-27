d6d62c222ec9bcd27e838d74bb096bcc964d136b
aaron <xietianle@outlook.com>
2023-05-27 10:43:20 +1000

    update ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="fixed bottom-0 w-full h-12 bg-gray-600">
+            <div className="text-gray-200">
+                Echo Atrium Chat
+            </div>
         </footer>
     );
 };
--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import config from '../config';
-import axiosInstance from "./axiosInstance";
 
 const Login = ({ onLogin }) => {
     const [username, setUsername] = useState('');
@@ -38,23 +37,23 @@ const Login = ({ onLogin }) => {
     }
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-blue-500">
-            <div className="bg-white p-8 md:p-16 rounded shadow-2xl w-full max-w-lg">
-                <h2 className="text-3xl font-bold mb-10 text-gray-800">Login</h2>
+        <div className="min-h-screen flex items-center justify-center bg-blue-500 p-4 w-full text-4xl lg:text-lg">
+            {/*<div className="bg-white w-full rounded shadow-2xl p-4 md:p-8 lg:p-16  mx-2 sm:mx-10 md:mx-0 md:max-w-xl">*/}
+            <div className="bg-white w-full rounded shadow-2xl p-4 md:p-8 lg:p-16 mx-2 sm:mx-4 md:mx-20 lg:max-w-xl">
+                <h2 className="text-3xl text-center font-bold mb-10 text-gray-800">Login</h2>
                 {error && <div className="bg-red-500 p-2 rounded-sm text-white mb-2">{error}</div>}
-                <div className="mb-5">
-                    <label htmlFor="username" className="block mb-2 text-sm font-bold text-gray-700">Username:</label>
-                    <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} className="w-full px-3 py-2 outline-none text-gray-700 focus:ring-2 focus:ring-blue-600 rounded-md" />
+                <div className="mb-8">
+                    <label htmlFor="username" className="block mb-2 font-bold text-gray-700">Username:</label>
+                    <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} className="w-full px-3 py-4 outline-none text-gray-700 focus:ring-2 focus:ring-blue-600 rounded-md" />
                 </div>
-                <div className="mb-5">
-                    <label htmlFor="password" className="block mb-2 text-sm font-bold text-gray-700">Password:</label>
-                    <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} className="w-full px-3 py-2 outline-none text-gray-700 focus:ring-2 focus:ring-blue-600 rounded-md" />
+                <div className="mb-8">
+                    <label htmlFor="password" className="block mb-2 font-bold text-gray-700">Password:</label>
+                    <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} className="w-full px-3 py-4 outline-none text-gray-700 focus:ring-2 focus:ring-blue-600 rounded-md" />
                 </div>
-                <button onClick={handleLogin} className="w-full p-3 mt-4 bg-sky-600 text-white rounded shadow hover:bg-sky-500">Login</button>
-                <p className="mt-8 text-center">Don't have an account? <Link to="/register" className="text-blue-500 hover:text-blue-700">Sign up here</Link>.</p>
+                <button onClick={handleLogin} className="w-full p-4 mt-4 lg:text-2xl bg-sky-600 text-white rounded shadow hover:bg-sky-500">Login</button>
+                <p className="mt-8 text-center text-2xl">Don't have an account? <Link to="/register" className="text-blue-500 hover:text-blue-700">Sign up here</Link>.</p>
             </div>
         </div>
-
     );
 };
 

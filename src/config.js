550255cca8377e4f5f2578a6b8872697164da486
aaron <xietianle@outlook.com>
2023-08-12 16:13:57 +1000

    update the urls

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 const config = {
-  BACKEND: process.env.PARCEL_BACKEND || "https://aaron404.com",
-  DJ_END: process.env.PARCEL_DJ_END || "https://aaron404.com",
+  BACKEND: process.env.PARCEL_BACKEND || "https://backend.aaron404.com",
+  DJ_END: process.env.PARCEL_DJ_END || "https://backend.aaron404.com",
   PEER_HOST: process.env.PARCEL_PEER_HOST || "peerjs.aaron404.com",
-  HOST: process.env.PARCEL_HOST || "aaron404.com",
-  SOCKET_HOST: process.env.PARCEL_SOCKET_HOST || "https://aaron404.com",
+  SOCKET_HOST: process.env.PARCEL_SOCKET_HOST || "https://backend.aaron404.com",
   CURRENT_HOST: process.env.PARCEL_CURRENT_HOST || "https://eac.aaron404.com",
 };
 
